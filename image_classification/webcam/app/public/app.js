f11420f3ac3c967799254335ef099dc020e593ff
Koji <kk2796@nyu.edu>
2019-01-02 19:42:04 -0500

    Update app.js
    remove lines for image

--- image_classification/webcam/app/public/app.js
+++ image_classification/webcam/app/public/app.js
@@ -1,17 +1,3 @@
-/*
-$('#image-selector').change(() => {
-    
-    reader.onload = () => {
-        let dataURL = reader.result;
-        $('#selected-image').attr("src", dataURL);
-        $("#prediction-list").empty();
-    }
-    let file = $('#image-selector').prop('files')[0];
-    reader.readAsDataURL(file);
-});
-*/
-
-
 // webcam
 let reader = new FileReader();
 const video = document.getElementById('video');
